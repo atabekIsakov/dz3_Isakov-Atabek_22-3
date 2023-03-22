7095acbe7a927a4f53e93747fe51fcc7daa2bdd0
SyimykMitalipove <syimyk310703@gmail.com>
2023-03-22 22:05:31 +0600

    lesson 3

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,25 +2,27 @@
 import React, { useState } from 'react'
 import classes from './modal.module.css'
 import Button from '../Button/Button'
+import Input from '../Input/Input'
 
-
-const Modal = ({ handleShow }) => {
-    const [ text, setText ]  = useState();
-    const handleChangeText = (textInput) => {
-        setText(textInput)
-        console.log(text, 'text');
-    }
+const Modal = ({ handleShow, handleChangeCheck, handleAdd  }) => {
+  
   return (
     <React.Fragment>
+
     <div className={classes.modalWrapper}></div>
     <div className={classes.modalContent}>
     <Button handleClick={handleShow}>Закрыть модалку</Button>
-    <input 
-    name='add'
-     placeholder='Добавьте таск'
-     onChange={(event) => handleChangeText(event.target.value)}
-      />
-    <Button>Добавить таск</Button>
+
+    <Input 
+    name={'add'}
+    onChangeFunc={handleChangeCheck}
+    />
+    <Button 
+    handleClick={handleAdd}
+    >Добавить таск</Button>
+
+
+
     </div>
     </React.Fragment>
   )
